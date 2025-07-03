7883d94223f414edc72ce0e8335afbdc3a816d87
Agus Setya <73156742+AgusSetyaNur@users.noreply.github.com>
2025-07-03 15:38:12 +0700

    almost finishing the game, change the flow and add set range feature

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 "use strict";
+let min = 1;
+let max = 10;
+let compNum = Math.floor(Math.random() * max) + min;
+let score = 20;
+let highScore = 0;
 
-const oriGuess = document.querySelector(".guess").cloneNode(true);
-let compNum = Math.floor(Math.random() * 19 + 1);
+function changeRange() {
+  restart();
+  min = Number(prompt("Enter the minimum number!"));
+  max = Number(prompt("Enter the maximum number!"));
+  if (isNaN(min) || isNaN(max) || min >= max) {
+    alert("Invalid range. Try again.");
+    return;
+  }
+  document.querySelector(
+    ".between"
+  ).textContent = `(Between ${min} and ${max})`;
+  compNum = Math.floor(Math.random() * (max - min) + min);
+}
+
+function displayMessage(msg) {
+  document.querySelector(".message").textContent = msg;
+}
+
 function execute() {
-  const comp = compNum;
   const guess = Number(document.querySelector(".guess").value);
-  let score = Number(document.querySelector(".score").textContent);
-  //   let highScore = score;
+
   if (!guess) {
-    document.querySelector(".message").textContent = "Enter your number first!";
-  } else if (guess === comp) {
-    document.querySelector(".message").textContent = "Correctttt";
-    document.querySelector(".number").textContent = comp;
-    let tempScore = Number(document.querySelector(".highscore").textContent);
-    let highScore = (document.querySelector(".highscore").textContent = score);
-    if (highScore < tempScore) {
-      document.querySelector(".highscore").textContent = tempScore;
+    displayMessage("Enter your number first!");
+  } else if (guess === compNum) {
+    displayMessage("Correctttt!");
+    document.querySelector(".number").textContent = compNum;
+    document.body.style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem ";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
     }
-  } else if (guess < comp) {
-    document.querySelector(".message").textContent = "its too low";
-    score--;
-    document.querySelector(".score").textContent = score;
   } else {
-    document.querySelector(".message").textContent = "its too big";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      displayMessage(guess < compNum ? "Too low!" : "Too high!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
   }
 }
-function restart() {
-  const reset = document
-    .querySelector(".guess")
-    .replaceWith(oriGuess.cloneNode(true));
-  document.querySelector(".guess").textContent = reset;
-}
-document.querySelector(".check").addEventListener("click", execute);
-document.querySelector(".again").addEventListener("reset", restart);
 
+function restart() {
+  score = 20;
+  compNum = Math.floor(Math.random() * 20) + 1;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+  displayMessage("Start guessing...");
+  document.body.style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem ";
+}
+document.querySelector(".range").addEventListener("click", changeRange);
+document.querySelector(".check").addEventListener("click", execute);
+document.querySelector(".again").addEventListener("click", restart);
